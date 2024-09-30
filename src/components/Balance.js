4834dcd5e085c0e9f1b9157d85e7fc656b2bc963
arthurdsfr <arthur.desfrancais@gmail.com>
2024-09-30 16:55:28 +0200

    balance with disconnection pb

---
+++ src/components/Balance.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+import '../css/Balance.css'; // Importer le fichier CSS pour le style
+
+const Balance = ({ contractAddress, abi, userAddress }) => {
+    const [balance, setBalance] = useState('0');
+
+    useEffect(() => {
+        const fetchBalance = async () => {
+            if (!userAddress) return; // Ne rien faire si l'utilisateur n'est pas connecté
+
+            const provider = new ethers.BrowserProvider(window.ethereum);
+            const contract = new ethers.Contract(contractAddress, abi, provider);
+
+            try {
+                const balance = await contract.balanceOf(userAddress);
+                setBalance(ethers.formatUnits(balance, 18)); // Formater en unités lisibles
+            } catch (error) {
+                console.error('Erreur lors de la récupération de la balance:', error);
+            }
+        };
+
+        fetchBalance();
+    }, [userAddress, contractAddress, abi]);
+
+    return (
+        <div className="balance-container">
+            <h3>Your balance : {balance} RUTRA</h3>
+        </div>
+    );
+};
+
+export default Balance;
